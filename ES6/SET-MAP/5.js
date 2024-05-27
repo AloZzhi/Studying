8997907dd3bec037f746623577fbad2536992366
AloZzhi <14421006+alozzhi@user.noreply.gitee.com>
2024-05-27 21:38:11 +0800

    'set和map'

--- ES6/SET-MAP/5.js
+++ ES6/SET-MAP/5.js
@@ -1,7 +1,22 @@
-const n = 123
+// const n = 123
+// const el = document.getElementById
 
 
-const obj = {
-  a: 1,  //a是字符串类型
-  [n]: 2
-}
+// const obj = {
+//   a: 1,  //a是字符串类型
+//   [n]: 2,
+//   [el]: 2
+// }
+
+
+const m = new Map()
+const o = { age: 18 }
+m.set(o, [1, 2, 3])
+console.log(m.get(o));//任意对象做key
+
+// m.delete(o)
+console.log(m.has(o));
+console.log(m.size);
+console.log(m.values());
+console.log(m.entries());
+
