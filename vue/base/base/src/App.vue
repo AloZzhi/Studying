8d43606a5becb5645fcb332348472936b1313b6e
AloZzhi <14421006+alozzhi@user.noreply.gitee.com>
2024-06-23 23:56:32 +0800

    'js'

--- vue/base/base/src/App.vue
+++ vue/base/base/src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div class="app">
-<!-- <h2>{{ title }}</h2>
-<h2 v-text="title2"></h2> 
-<h3 v-html="content"></h3>-->
-<button v-on:click="add" @mousemove="move">{{ count }}</button>
+  </div>
+
+  <div class="main">
+    <h2 v-show="isEn">{{ title1 }}</h2>
+    <button @click="change">切换</button>
   </div>
 </template>
 
 <script setup>
-// const title = 'Hello World'
-// const title2 = '标题'
-// const content ='<p>床前明月光</p><p>疑是地上霜</p>'
-import {ref} from 'vue'
+import { ref } from 'vue'
+let title1 = 'Hello'
+let title2 = '你好'
+let isEn = ref(true)
 
-let count =ref(0)//要声明称响应式的
-  const add =()=>{
-  count.value++
+
+const change = () => {
+  isEn.value = !isEn.value
 }
-
-const move =()=>{
-  console.log('鼠标在button上移动');
-}
-
 </script>
 
-<style lang="css" scoped>
-
-</style>
+<style lang="css" scoped></style>
