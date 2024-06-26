acb31d8ad8b1ad002e6d3fd2d9f5c58fdc24381b
AloZzhi <14421006+alozzhi@user.noreply.gitee.com>
2024-06-26 21:28:25 +0800

    'vue-sell'

---
+++ vue/vue-sell/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header />
+</template>
+
+<script setup>
+import Header from '@/components/header/Index.vue'
+
+
+// axios request seller
+
+</script>
+
+<style lang="less" scoped>
+div {
+  font-size: 20px;
+}
+</style>
