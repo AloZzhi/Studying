f8e251f0ad796663399466d1210980adf939b346
AloZzhi <14421006+alozzhi@user.noreply.gitee.com>
2024-07-13 21:50:55 +0800

    'stack'

--- leetCode/栈/1.js
+++ leetCode/栈/1.js
@@ -1,9 +1,13 @@
 // map 和 forEach的区别就是map有返回值
 
-const arr = new Array(8)
+const arr = new Array(4).fill(9)
 // console.log(arr);
 
-arr.forEach(function (item, index, arr) {
 
+
+const newArr = arr.map(function (item, index, arr) {
     console.log(item, index, arr);
+    return item + 1
 });
+
+console.log(newArr);
--- leetCode/栈/2.js
+++ leetCode/栈/2.js
@@ -1,17 +1,17 @@
 const arr = [1, 2]
 
-arr.push(3)
-arr.unshift(0)
+arr.push(3)//尾部增加（会直接修改原来数组）
+arr.unshift(0)//头部增加
 console.log(arr);
-arr.pop()
-arr.shift()
+arr.pop()//移除尾部
+arr.shift()//移除头部
 console.log(arr);
 // 数组中增加元素 第一参数是位置，第二个是要删几个 第三个参数是插入的元素
 arr.splice(1, 0, 'hello')
 console.log(arr);
 arr.splice(1, 1)
 console.log(arr);
-console.log('--------------------------------------------------------');
-const arr1 = [1, 2, 3]
-// 栈
-const stack = []
+// console.log('--------------------------------------------------------');
+// const arr1 = [1, 2, 3]
+// // 栈
+// const stack = []
