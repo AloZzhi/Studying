7e676d6e1cbad6076861316fefc88f977908129e
AloZzhi <14421006+alozzhi@user.noreply.gitee.com>
2024-08-10 22:13:12 +0800

    ''

--- leetCode/排序/2选择排序.js
+++ leetCode/排序/2选择排序.js
@@ -28,14 +28,14 @@
 var len= arr.length
 var minIndex,temp
 for(var i=0;i<len-1;i++){
-  minIndex =i
+  minIndex =i;
   for(var j=i+1;j<len;j++){
     if(arr[j]<arr[minIndex]){
-      minIndex = j
+      minIndex = j;
     }
-    temp = arr[i]
-    arr[i] = arr[minIndex]
-    arr[minIndex] = temp
+    temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
   }
-  return arr
+  return arr;
 }
