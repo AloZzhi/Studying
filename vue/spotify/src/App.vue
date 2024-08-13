09a35b025f6439b89b71e8de3bf7fe54e23c5d80
AloZzhi <14421006+alozzhi@user.noreply.gitee.com>
2024-08-13 22:43:36 +0800

    '项目'

--- vue/spotify/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-
-</script>
-
-
-<template>
-<div class="bg-red-400">COOL</div>
-
-</template>
---
+++ vue/项目/spotify/src/views/Homeview.vue
@@ -0,0 +1,43 @@
+<script setup>
+import HomeCard from '../components/HomeCard.vue';
+</script>
+
+<template>
+  <div class="p-8">
+    <button type="button" class="text-white text-2xl font-semibold hover:underline cursor-pointer">
+      Podcasts to make you smarter
+    </button>
+    <div class="py-1.5"></div>
+    <!-- 使用picsum.photo随机图库 -->
+    <div class="flex items-center">
+      <HomeCard image="http://picsum.photos/id/30/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard image="http://picsum.photos/id/45/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard class="md:block hidden" image="http://picsum.photos/id/60/300/300" title="Title is here"
+        subTitle="Subtitle is here" />
+      <HomeCard class="lb:block hidden" image="http://picsum.photos/id/67/300/300" title="Title is here"
+        subTitle="Subtitle is here" />
+      <HomeCard class="xl:block hidden" image="http://picsum.photos/id/100/300/300" title="Title is here"
+        subTitle="Subtitle is here" />
+    </div>
+  </div>
+
+
+  <div class="p-8">
+    <button type="button" class="text-white text-2xl font-semibold hover:underline cursor-pointer">
+      Recommended songs
+    </button>
+
+    <div class="py-1.5"></div>
+
+    <div class="flex items-center">
+      <HomeCard image="https://picsum.photos/id/88/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard image="https://picsum.photos/id/243/300/300" title="Title is here" subTitle="Subtitle is here" />
+      <HomeCard class="md:block hidden" image="https://picsum.photos/id/123/300/300" title="Title is here"
+        subTitle="Subtitle is here" />
+      <HomeCard class="lg:block hidden" image="https://picsum.photos/id/121/300/300" title="Title is here"
+        subTitle="Subtitle is here" />
+      <HomeCard class="xl:block hidden" image="https://picsum.photos/id/99/300/300" title="Title is here"
+        subTitle="Subtitle is here" />
+    </div>
+  </div>
+</template>
